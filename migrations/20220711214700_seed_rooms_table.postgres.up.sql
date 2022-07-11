a560b4ccd16fe9b55026b3f78ab1da78abd8e1b9
Wanda Wang <chuwang@wustl.edu>
2022-07-11 14:49:52 -0700

    Add seed for rooms and restrictions table

---
+++ migrations/20220711214700_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+                                                               ('General''s Quarters','2022-07-10 00:00:00','2022-07-10 00:00:00'),
+                                                               ('Major''s Suite','2022-07-10 00:00:00','2022-07-10 00:00:00');
---
+++ migrations/20220711214845_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+                                                                             ('Reservation','2022-07-01 00:00:00','2022-07-01 00:00:00'),
+                                                                             ('Owner Block','2022-07-01 00:00:00','2022-07-01 00:00:00');
